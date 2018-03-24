54055dddc5467b5d8e1f8bc0204e161cc11606b1
Dexter <franciscoreynag@gmail.com>
2018-03-24 00:52:12 -0300

    Header Description

--- Control/Quanser Rotary Servo Base Unit srv02/potentiometer.h
+++ Control/Quanser Rotary Servo Base Unit srv02/potentiometer.h
@@ -2,12 +2,12 @@
 #define __potentiometer_H_INCLUDED__
 
 /*POTENTIOMETER VALUE*/
-inline uint16_t potentiometer_get_value();  // RETURNS DEGREE VALUE FROM REFERENCE
+inline uint16_t potentiometer_get_value();  // VALUE MIGHT BE IN RADIANS/DEGREES DEPENDING ON CONFIG (DEFAULT: RADIANS)
 bool potentiometer_is_radians(bool radians);
 
 /*SETUP*/
 bool potentiometer_set(uint8_t potentiometer_pin, bool set_zero_reference, float voltage, uint16_t resolution, bool radians);
-bool potentiometer_set(uint8_t potentiometer_pin, bool set_zero_reference);  // ASSUMES DEFAULT VALUES (5V, 1024, true)
+bool potentiometer_set(uint8_t potentiometer_pin, bool set_zero_reference);  // ASSUMES DEFAULT VALUES (5V, 1024, true) 
 bool potentiometer_set_zero_reference();  // THE REFERENCE VALUE IS SET AT ACTUAL MOTOR POSITION
 bool potentiometer_set_zero_reference(uint16_t value);  // THE REFERENCE VALUE IS SET TO A SPECIFIED VALUE
 bool potentiometer_set_radians(bool radians);
