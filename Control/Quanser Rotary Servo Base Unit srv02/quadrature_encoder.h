e119056efb2a976520f858b3a9c16106df7c5b1c
Dexter <franciscoreynag@gmail.com>
2018-03-24 03:12:39 -0300

    Encoder Refactor

--- Control/Quanser Rotary Servo Base Unit srv02/quadrature_encoder.h
+++ Control/Quanser Rotary Servo Base Unit srv02/quadrature_encoder.h
@@ -2,24 +2,15 @@
 #define __quadrature_encoder_H_INCLUDED__
 
 /*POSITION DATA RELATED*/
-
-//RETURN THE ENCODER VALUE
-inline int16_t get_encoder_value();
-//SET THE ENCODER VALUE OF REFERENCE
-bool set_encoder_reference_value(int16_t value);
-//SET ZERO REFERENCE FOR THE ENCODER
-bool set_encoder_zero_reference();
-//RETURN TRUE IF IT IS CLOCKWISE
-bool get_turn_status();
-
+inline int16_t encoder_get_value();
+bool encoder_is_turning_clockwise();
 
 /*ENCODER SETUP*/
-
-// RUN set_encoder_pins,set_encoder_limit AND active_encoder IN ORDER TO SET ACTIVE THE ENCODER
-bool set_encoder(uint8_t clockwise_pin, uint8_t counterclockwise_pin, int16_t max_value);
-// SET PINS AS INPUTS AND ATTACH INTTERUPTS
-bool active_encoder();
-// DETACH INTERRUPTS, RETURN TRUE IF THE ENCODER WAS ACTIVE AND COULD BE DEACTIVATED
-bool deactivate_encoder();
+bool encoder_set(uint8_t clockwise_pin, uint8_t counterclockwise_pin, uint16_t max_value);
+bool encoder_set(uint8_t clockwise_pin, uint8_t counterclockwise_pin);  // ASSUMES DEFAULT VALUE (4096)
+bool encoder_set_zero_reference();  // SET THE ACTUAL MOTOR POSITION AS THE ZERO REFERENCE FOR THE ENCODER
+bool encoder_set_reference_value(int16_t value); // SET AN OFFSET FROM THE ACTUAL MOTOR POSITION AS THE ZERO REFERENCE FOR THE ENCODER
+bool encoder_activate();  // SET PINS AS INPUTS AND ATTACH INTTERUPTS. (MUST SET ENCODER FIRST)
+bool encoder_deactivate();  // DETACH INTERRUPTS
 
 #endif
