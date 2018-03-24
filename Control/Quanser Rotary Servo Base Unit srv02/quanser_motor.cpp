694bede0fb548b7bd14febaa0dbc8adbe5dbc489
Dexter <franciscoreynag@gmail.com>
2018-03-24 01:41:45 -0300

    Motor Update

--- Control/Quanser Rotary Servo Base Unit srv02/quanser_motor.cpp
+++ Control/Quanser Rotary Servo Base Unit srv02/quanser_motor.cpp
@@ -1,9 +1,44 @@
 #include <quanser_motor.h>
-bool motor_is_set = false;
+/*HARDWARE INPUT*/
 uint8_t motor_clockwise_pin, motor_counterclockwise_pin;
-//HEADER
-inline void motor_move(uint8_t active_motor, uint8_t pasive_motor);
 
+/*CONDITIONS*/
+bool motor_is_set = false;
+
+// FUNCTIONS DEFINITIONS
+
+//HEADER
+bool motor_move(uint8_t active_motor, uint8_t pasive_motor);
+
+/*MOTOR MOVE*/
+bool motor_move(bool move_clockwise){
+    if (move_clockwise){
+      motor_move(motor_clockwise_pin, motor_counterclockwise_pin);
+    }
+    else{
+      motor_move(motor_counterclockwise_pin, motor_clockwise_pin);
+    }
+}
+
+bool motor_move(uint8_t active_motor, uint8_t pasive_motor){
+  if (motor_is_set) {
+    digitalWrite(pasive_motor, LOW);
+    digitalWrite(active_motor, HIGH);
+    return true;
+  }
+  return false;
+}
+
+/*MOTOR MOVE*/
+bool motors_stop(){
+  if (motor_is_set) {
+    digitalWrite(motor_clockwise_pin, LOW);
+    digitalWrite(motor_counterclockwise_pin, LOW);
+  }
+  return true;
+}
+
+/*MOTOR SETUP*/
 bool motor_set_pins(uint8_t clockwise_pin, uint8_t counterclockwise_pin){
   if(clockwise_pin != counterclockwise_pin){
       motor_clockwise_pin = clockwise_pin;
@@ -14,29 +49,4 @@ bool motor_set_pins(uint8_t clockwise_pin, uint8_t counterclockwise_pin){
       return true;
   }
   return false;
-}
-
-inline void motor_move(uint8_t active_motor, uint8_t pasive_motor){
-  if (motor_is_set) {
-    digitalWrite(pasive_motor, LOW);
-    digitalWrite(active_motor, HIGH);
-  }
-}
-
-inline void motor_move(bool move_clockwise){
-  if (motor_is_set) {
-    if (move_clockwise){
-      motor_move(motor_clockwise_pin, motor_counterclockwise_pin);
-    }
-    else{
-      motor_move(motor_counterclockwise_pin, motor_clockwise_pin);
-    }
-  }
-}
-
-inline void motors_stop(){
-  if (motor_is_set) {
-    digitalWrite(motor_clockwise_pin, LOW);
-    digitalWrite(motor_counterclockwise_pin, LOW);
-  }
 }
--- Control/Quanser Rotary Servo Base Unit srv02/quanser_motor.h
+++ Control/Quanser Rotary Servo Base Unit srv02/quanser_motor.h
@@ -2,10 +2,11 @@
 #define __quadrature_encoder_H_INCLUDED__
 
 /*MOVE MOTORS*/
-inline void motor_move(bool move_clockwise);
+bool motor_move(bool move_clockwise);
 /*STOP MOTORS*/
-inline void motors_stop();
+bool motors_stop();
 
 /*SETUP MOTOR PINS*/
 bool motor_set_pins(uint8_t clockwise_pin, uint8_t counterclockwise_pin);
+
 #endif
