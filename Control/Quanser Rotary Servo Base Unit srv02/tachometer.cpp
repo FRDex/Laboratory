0496c6f6e002f8abbba7a8ad072117d85ec0955e
Dexter <franciscoreynag@gmail.com>
2018-03-24 01:32:37 -0300

    Tachometer Refactor

--- Control/Quanser Rotary Servo Base Unit srv02/tachometer.cpp
+++ Control/Quanser Rotary Servo Base Unit srv02/tachometer.cpp
@@ -1,54 +1,52 @@
 #include <tachometer.h>
-
+// REFERENCES
 #ifndef __PI_DEFINED__
 #define __PI_DEFINED__
   #define PI (float)(3.14159265358979)
 #endif
 
-//HARDWARE INPUT
+/*HARDWARE INPUT*/
 uint8_t tachometer_pin;
 
-//CALIBRATION VALUES
+/*CONDITIONS*/
 bool tachometer_is_set = false;
+bool is_radian = true;
+
+/*CALIBRATION VALUES*/
 uint16_t tachometer_zero_ref = 0;
 float voltage = 5.0;
 uint16_t resolution = 1024;
-bool is_radian = true;
 uint16_t tachometer_sensitivity = 1500; // rpm/V
-float tachometer_scale = (2 * PI * voltage * tachometer_sensitivity) / 60;
+float tachometer_scale = (2 * PI * voltage * tachometer_sensitivity) / (60 * resolution);
 
+// FUNCTIONS DEFINITIONS
+
+/*TACHOMETER VALUE*/
+uint16_t tachometer_get_value(){
+  return (analogRead(tachometer_pin) - tachometer_zero_ref) * tachometer_scale;
+}
 
 bool tachometer_is_radians(){
     return(is_radian);
 }
 
-bool tachometer_set_radians(bool radians){
-    is_radian = radians;
-    if(is_radian){
-        tachometer_scale = (2 * PI * voltage * tachometer_sensitivity) / 60;
-    }
-    else{
-        tachometer_scale = (360 * voltage * tachometer_sensitivity) / 60;
-    }
-
-}
-
-bool tachometer_set(uint8_t tachometer_pin, bool set_zero_reference, float voltage, uint16_t resolution, bool radians, uint16_t tachometer_sensitivity){
+/*SETUP*/
+bool tachometer_set(uint8_t tachometer_pin, bool set_zero_reference, bool radians, float voltage, uint16_t resolution, uint16_t tachometer_sensitivity){
+  this->tachometer_pin = tachometer_pin;
   this->voltage = voltage;
   this->resolution = resolution;
   this->tachometer_sensitivity = tachometer_sensitivity;
+  pinMode(tachometer_pin, INPUT);
   tachometer_set_radians(radians);
-  return tachometer_set(tachometer_pin, set_zero_reference);
+  if(set_zero_reference){
+    tachometer_set_zero_reference();
+  }
+  tachometer_is_set = true;
+  return tachometer_is_set;
 }
 
 bool tachometer_set(uint8_t tachometer_pin, bool set_zero_reference){
-      this->tachometer_pin = tachometer_pin;
-      pinMode(tachometer_pin, INPUT);
-      if(set_zero_reference){
-        tachometer_set_zero_reference();
-      }
-      tachometer_is_set = true;
-      return tachometer_is_set;
+      return tachometer_set(tachometer_pin, set_zero_reference, true, 5.0, 1024, 1500);
 }
 
 bool tachometer_set_zero_reference(){
@@ -56,6 +54,18 @@ bool tachometer_set_zero_reference(){
   return true;
 }
 
-inline uint16_t tachometer_get_value(){
-  return ((analogRead(tachometer_pin) - tachometer_zero_ref) * tachometer_scale) / resolution;
+bool tachometer_set_zero_reference(uint16_t value){
+  tachometer_zero_ref = value;
+  return true;
+}
+
+bool tachometer_set_radians(bool radians){
+    is_radian = radians;
+    if(is_radian){
+        tachometer_scale = (2 * PI * voltage * tachometer_sensitivity) / (60 * resolution);
+    }
+    else{
+        tachometer_scale = (360 * voltage * tachometer_sensitivity) / (60 * resolution);
+    }
+    return is_radian;
 }
--- Control/Quanser Rotary Servo Base Unit srv02/tachometer.h
+++ Control/Quanser Rotary Servo Base Unit srv02/tachometer.h
@@ -1,12 +1,14 @@
 #ifndef __tachometer_H_INCLUDED__
 #define __tachometer_H_INCLUDED__
 
-/*tachometer VALUE*/
-inline uint16_t tachometer_get_value(); //RETURNS DEGREE VALUE FROM REFERENCE
-bool tachometer_is_radians(bool radians);
+/*TACHOMETER VALUE*/
+uint16_t tachometer_get_value();  // VALUE MIGHT BE IN RADIANS/DEGREES DEPENDING ON CONFIG (DEFAULT: RADIANS)
+bool tachometer_is_radians();
 /*SETUP */
-bool tachometer_set(uint8_t tachometer_pin, bool set_zero_reference);
-bool tachometer_set(uint8_t tachometer_pin, bool set_zero_reference, float voltage, uint16_t resolution, bool radians, uint16_t tachometer_sensitivity);
+bool tachometer_set(uint8_t tachometer_pin, bool set_zero_reference, bool radians, float voltage, uint16_t resolution, uint16_t tachometer_sensitivity);
+bool tachometer_set(uint8_t tachometer_pin, bool set_zero_reference);  // ASSUMES DEFAULT VALUES (true, 5V, 1024, 1500)
+bool tachometer_set_zero_reference();  // THE REFERENCE VALUE IS SET AT ACTUAL MOTOR STATE
+bool tachometer_set_zero_reference(uint16_t value);  // THE REFERENCE VALUE IS SET TO A SPECIFIED VALUE
 bool tachometer_set_radians(bool radians);
 
 #endif
