371264c7470dc3a866aef4e28f424971a2ed6eee
Dexter <franciscoreynag@gmail.com>
2018-03-24 00:22:05 -0300

    Potentiometer Refactor
    Implementation changes were implamented, (constructor refactor, potenciometer_scale)

--- Control/Quanser Rotary Servo Base Unit srv02/potentiometer.cpp
+++ Control/Quanser Rotary Servo Base Unit srv02/potentiometer.cpp
@@ -1,51 +1,50 @@
 #include <potentiometer.h>
-
+// REFENCES
 #ifndef __PI_DEFINED__
 #define __PI_DEFINED__
   #define PI (float)(3.14159265358979)
 #endif
-//HARDWARE INPUT
+
+/*HARDWARE INPUT*/
 uint8_t potentiometer_pin;
 
-//CALIBRATION VALUES
+/*CONDITIONS*/
+bool is_radian = true;
 bool potentiometer_is_set = false;
+
+/*CALIBRATION VALUES*/
 uint16_t potentiometer_zero_ref = 0;
 float voltage = 5.0;
 uint16_t resolution = 1024;
-bool is_radian = true;
-float potentiometer_scale = 2 * PI * voltage * tachometer_sensitivity;
+float potentiometer_scale = (2 * PI * voltage * tachometer_sensitivity) / resolution;
 
+// FUNCTIONS DEFINITIONS
+
+/*POTENTIOMETER VALUE*/
+inline uint16_t potentiometer_get_value(){
+  return (analogRead(potentiometer_pin) - potentiometer_zero_ref) * potentiometer_scale;
+}
 
 bool potentiometer_is_radians(){
     return(is_radian);
 }
 
-bool potentiometer_set_radians(bool radians){
-    is_radian = radians;
-    if(is_radian){
-        potentiometer_scale = 2 * PI * voltage;
-    }
-    else{
-        potentiometer_scale = 360 * voltage;
-    }
-
-}
-
+/*SETUP*/
 bool potentiometer_set(uint8_t potentiometer_pin, bool set_zero_reference, float voltage, uint16_t resolution, bool radians){
+  this->potentiometer_pin = potentiometer_pin;
   this->voltage = voltage;
   this->resolution = resolution;
+  pinMode(potentiometer_pin, INPUT);
   potentiometer_set_radians(radians);
-  return potentiometer_set(potentiometer_pin, set_zero_reference);
+  if(set_zero_reference){
+    potentiometer_set_zero_reference();
+  }
+  potentiometer_is_set = true;
+  return potentiometer_is_set;
 }
 
 bool potentiometer_set(uint8_t potentiometer_pin, bool set_zero_reference){
-      this->potentiometer_pin = potentiometer_pin;
-      pinMode(potentiometer_pin, INPUT);
-      if(set_zero_reference){
-        potentiometer_set_zero_reference();
-      }
-      potentiometer_is_set = true;
-      return potentiometer_is_set;
+      return potentiometer_set(potentiometer_pin, set_zero_reference, 5.0, 1024, true);
 }
 
 bool potentiometer_set_zero_reference(){
@@ -53,6 +52,17 @@ bool potentiometer_set_zero_reference(){
   return true;
 }
 
-inline uint16_t potentiometer_get_value(){
-  return ((analogRead(potentiometer_pin) - potentiometer_zero_ref) * potentiometer_scale) / resolution;
+bool potentiometer_set_zero_reference(uint16_t value){
+  potentiometer_zero_ref = value;
+  return true;
+}
+
+bool potentiometer_set_radians(bool radians){
+    is_radian = radians;
+    if(is_radian){
+        potentiometer_scale = (2 * PI * voltage) / resolution;
+    }
+    else{
+        potentiometer_scale = (360 * voltage) / resolution;
+    }
 }
--- Control/Quanser Rotary Servo Base Unit srv02/potentiometer.h
+++ Control/Quanser Rotary Servo Base Unit srv02/potentiometer.h
@@ -2,12 +2,14 @@
 #define __potentiometer_H_INCLUDED__
 
 /*POTENTIOMETER VALUE*/
-inline uint16_t potentiometer_get_value(); //RETURNS DEGREE VALUE FROM REFERENCE
+inline uint16_t potentiometer_get_value();  // RETURNS DEGREE VALUE FROM REFERENCE
 bool potentiometer_is_radians(bool radians);
-/*SETUP */
-bool potentiometer_set(uint8_t potentiometer_pin, bool set_zero_reference);
+
+/*SETUP*/
 bool potentiometer_set(uint8_t potentiometer_pin, bool set_zero_reference, float voltage, uint16_t resolution, bool radians);
+bool potentiometer_set(uint8_t potentiometer_pin, bool set_zero_reference);  // ASSUMES DEFAULT VALUES (5V, 1024, true)
+bool potentiometer_set_zero_reference();  // THE REFERENCE VALUE IS SET AT ACTUAL MOTOR POSITION
+bool potentiometer_set_zero_reference(uint16_t value);  // THE REFERENCE VALUE IS SET TO A SPECIFIED VALUE
 bool potentiometer_set_radians(bool radians);
-bool potentiometer_set_zero_reference();
 
 #endif
