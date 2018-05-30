f3ceb7c13e23df8ac4a483ba794f6298cfe00edb
Dexter <franciscoreynag@gmail.com>
2018-05-30 03:36:04 -0300

    Struct Differentiation Implementation

--- Control/Quanser Rotary Servo Base Unit srv02/PID/differentiation.c
+++ Control/Quanser Rotary Servo Base Unit srv02/PID/differentiation.c
@@ -1,125 +1,111 @@
 #include <differentiation.h>
-/*DEFINES*/
-define MAX_POINTS 4
-define MIN_POINTS 1
-/*CONDITIONALS*/
-static bool is_differential_set = false;
-/*ARRAY STRUCTURE INFO*/
-static float last_value = 0
-/*DIFFENTIATIAL VARIABLES*/
-static uint8_t n_differential_points;
-static uint8_t differential_degree;
-static float derivative;
-
-static float differential_values[MAX_POINTS];
-static float differential_separation[MAX_POINTS];
-//static uint8_t differential_index[MAX_POINTS];
 
 /*FUNCTION DECLARATION*/
-void shift_values();
-//void increase_indexes();
+void shift_values(differentiation_struct & differentiation_structure);
 
 /*DIFFENTIATIAL FUNCTIONS*/
-float differential_get_value(){
-  return derivative;
+float differential_get_value(differentiation_struct & differentiation_structure){
+  return differentiation_structure.derivative;
 }
 
 //NUMERICAL FORWARD DIFFENTIATION TO APROXIMATE THE DERIVATIVE
-float differential_new_value(float value, float sample_separation){
-  differential_degree = (differential_degree > n_differential_points) ? n_differential_points : differential_degree;
+float differential_new_value(float value, float sample_separation, differentiation_struct & differentiation_structure){
+  differentiation_structure.differential_degree = (differentiation_structure.differential_degree > differentiation_structure.differential_points) ? differentiation_structure.differential_points : differentiation_structure.differential_degree;
 
-  switch (differential_degree) {
+  switch (differentiation_structure.differential_degree) {
     case 0:{
-      derivative = 0;
+      differentiation_structure.derivative = 0;
     break;
     }
     case 1:{
-      derivative = (value - last_value) / sample_separation;
+      differentiation_structure.derivative = (value - differentiation_structure.previous_value) / sample_separation;
     break;
     }
     case 2:{
       // FACTORS FOR CALCULATE THE DERIVATIVE
       static const float factor_2 = 1.0 / 3.0;
 
-      derivative = 2.0 * (value - last_value) / sample_separation;
-      derivative += differential_values[1];
-      derivative *= factor_2;
+      differentiation_structure.derivative = 2.0 * (value - differentiation_structure.previous_value) / sample_separation;
+      differentiation_structure.derivative += differentiation_structure.differential_values[1];
+      differentiation_structure.derivative *= factor_2;
     }
     case 3:{
       // FACTORS FOR CALCULATE THE DERIVATIVE
       static const float factor_3 = 1.0 / 8.0;
-      float sample_separation_ratio_1 = sample_separation / differential_separation[1];
+      float sample_separation_ratio_1 = sample_separation / differentiation_structure.differential_separation[1];
 
-      derivative = 6.0 * (value - last_value) / sample_separation;
-      derivative += (2 - sample_separation_ratio_1) * differential_values[1];
-      derivative += sample_separation_ratio_1 * differential_values[2];
-      derivative *= factor_3;
+      differentiation_structure.derivative = 6.0 * (value - differentiation_structure.previous_value) / sample_separation;
+      differentiation_structure.derivative += (2 - sample_separation_ratio_1) * differentiation_structure.differential_values[1];
+      differentiation_structure.derivative += sample_separation_ratio_1 * differentiation_structure.differential_values[2];
+      differentiation_structure.derivative *= factor_3;
     break;
     }
     case 4: default:{
       // FACTORS FOR CALCULATE THE DERIVATIVE
       static const float factor_4 = 1.0 / 33.0;
-      float sample_separation_ratio_1 = sample_separation / differential_separation[1];
+      float sample_separation_ratio_1 = sample_separation / differentiation_structure.differential_separation[1];
       float sample_separation_ratio_1_1 = sample_separation_ratio_1 * sample_separation_ratio_1;
-      float sample_separation_ratio_2 = sample_separation / differential_separation[2];
+      float sample_separation_ratio_2 = sample_separation / differentiation_structure.differential_separation[2];
       float sample_separation_ratio_1_2 = sample_separation_ratio_1 * sample_separation_ratio_2;
 
-      derivative = 24.0 * (value - last_value) / sample_separation;
-      derivative += (9 - 3 * sample_separation_ratio_1 + sample_separation_ratio_1_1) * differential_values[1];
-      derivative += (3 * sample_separation_ratio_1 - sample_separation_ratio_1_1 - sample_separation_ratio_1_2) * differential_values[2];
-      derivative += sample_separation_ratio_1_2 * differential_values[2];
-      derivative *= factor_4;
+      differentiation_structure.derivative = 24.0 * (value - differentiation_structure.previous_value) / sample_separation;
+      differentiation_structure.derivative += (9 - 3 * sample_separation_ratio_1 + sample_separation_ratio_1_1) * differentiation_structure.differential_values[1];
+      differentiation_structure.derivative += (3 * sample_separation_ratio_1 - sample_separation_ratio_1_1 - sample_separation_ratio_1_2) * differentiation_structure.differential_values[2];
+      differentiation_structure.derivative += sample_separation_ratio_1_2 * differentiation_structure.differential_values[2];
+      differentiation_structure.derivative *= factor_4;
     break;
     }
   }
-  differential_separation[0] = sample_separation;
-  differential_values[0] = derivative;
-  last_value = value;
-  ++differential_degree;
-  shift_values();
-  //increase_indexes();
+  differentiation_structure.differential_separation[0] = sample_separation;
+  differentiation_structure.differential_values[0] = derivative;
+  differentiation_structure.previous_value = value;
+  ++differentiation_structure.differential_degree;
+  shift_values(differentiation_structure);
 
-  return derivative;
+  return differentiation_structure.derivative;
 }
 
 /*DIFFENTIATIAL SETUP*/
-bool differential_setup(uint8_t differential_points){
+bool differential_setup(uint8_t differential_points, differentiation_struct & differentiation_structure){
   is_differential_set = false;
   if(differential_points > 1){
-    is_differential_set = differential_clear(differential_points);
+    differentiation_structure.differential_points = differential_points;
+    is_differential_set = differential_clear(differentiation_structure);
   }
 	return is_differential_set;
 }
 
-bool differential_clear(uint8_t differential_points){
-  n_differential_points = (differential_points >= MIN_POINTS) ? differential_points : MIN_POINTS;
-  n_differential_points = (n_differential_points >= MAX_POINTS) ? MAX_POINTS : n_differential_points;
-  differential_degree = 0;
-  last_value = 0
-  for(uint8_t k = 0; k < n_differential_points; ++k){
-    differential_values[k] = 0;
-    differential_separation[k] = 0;
-    //differential_index[k] = n_differential_points - 1 - k;
+bool differential_clear(differentiation_struct & differentiation_structure){
+  differentiation_structure.differential_points = (differential_points >= DIFFERENTIATION_MIN_POINTS) ? differentiation_structure.differential_points : DIFFERENTIATION_MIN_POINTS;
+  differentiation_structure.differential_points = (differentiation_structure.differential_points >= DIFFERENTIATION_MAX_POINTS) ? DIFFERENTIATION_MAX_POINTS : differentiation_structure.differential_points;
+
+  differentiation_structure.differential_degree = 0;
+  differentiation_structure.previous_value = 0
+  for(uint8_t k = 0; k < differentiation_structure.differential_points; ++k){
+    differentiation_structure.differential_values[k] = 0;
+    differentiation_structure.differential_separation[k] = 0;
   }
   return true;
 }
 
-void shift_values(){
-  for(uint8_t actual_index = n_differential_points - 2, new_index = n_differential_points - 1; new_index > 0; --actual_index, --new_index){
-    differential_values[new_index] = differential_values[actual_index];
-    differential_separation[new_index] = differential_separation[actual_index];
+void shift_values(differentiation_struct & differentiation_structure){
+  for(uint8_t actual_index = differentiation_structure.differential_points - 2, new_index = differentiation_structure.differential_points - 1; new_index > 0; --actual_index, --new_index){
+    differentiation_structure.differential_values[new_index] = differentiation_structure.differential_values[actual_index];
+    differentiation_structure.differential_separation[new_index] = differentiation_structure.differential_separation[actual_index];
   }
 }
+
 
 /*IT INCREASE INDEXES TO AVOID SHIFTING THE ARRAY */
 /* ANOTHER IMPLEMENTATION TO AVOID SHIFTING, BUT IN THIS CASE I THINK THAT
 IT WILL BE CLEARER TO SHIFT ARRAYS
+//static uint8_t differential_index[DIFFERENTIATION_MAX_POINTS];
 void increase_indexes(){
   uint8_t new_index =  differential_index[0] + 1;
-  new_index = (new_index >= MAX_POINTS) ? 0 : new_index;
-  for (uint8_t k = 0, index = new_index; k < n_differential_points; ++k, --index){
+  new_index = (new_index >= DIFFERENTIATION_MAX_POINTS) ? 0 : new_index;
+  for (uint8_t k = 0, index = new_index; k < differential_points; ++k, --index){
     if(index < 0){
-      index = n_differential_points - 1;
+      index = differential_points - 1;
     }
     differential_index[k] = index;
   }
--- Control/Quanser Rotary Servo Base Unit srv02/PID/differentiation.h
+++ Control/Quanser Rotary Servo Base Unit srv02/PID/differentiation.h
@@ -1,11 +1,28 @@
 #ifndef __DIFFERENTIATION_H_INCLUDED__
 #define __DIFFERENTIATION_H_INCLUDED__
 
+/*DEFINES*/
+define DIFFERENTIATION_MAX_POINTS 4
+define DIFFERENTIATION_MIN_POINTS 1
+
+/*DIFFERENTIATION STRUCT*/
+typedef struct {
+  /*CONDITIONALS*/
+  bool is_differential_set;
+  /*DIFFERENTIATION VARIABLES*/
+  uint8_t differential_degree, differential_points;
+  float previous_value;
+  float derivative;
+  /*ARRAY STRUCTURE INFO*/
+  float differential_values[DIFFERENTIATION_MAX_POINTS];
+  float differential_separation[DIFFERENTIATION_MAX_POINTS];
+} differentiation_struct;
+
 /*INTEGRAL VALUE*/
-float differential_get_value();
-float differential_new_value(float value, float sample_separation);
+float differential_get_value(differentiation_struct & differentiation_structure);
+float differential_new_value(float value, float sample_separation, differentiation_struct & differentiation_structure);
 /*INTEGRAL SETUP*/
-bool differential_setup(uint8_t differential_points);
-bool differential_clear(uint8_t differential_points);
+bool differential_setup(uint8_t differential_points, differentiation_struct & differentiation_structure);
+bool differential_clear(differentiation_struct & differentiation_structure);
 
 #endif
